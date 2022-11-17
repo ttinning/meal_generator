ebf967c41f83a9e31fd1c5baed04a38f4274d0f0
Tristan Tinning <tristan_tinning@hotmail.com>
2022-11-17 08:50:33 +0000

    homepage with footer before css complete

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faTwitter,
+    faInstagramSquare,
+} from '@fortawesome/free-brands-svg-icons';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <section> 
+                <img className="logo"></img>
+            </section>
+            <section className="social">
+                <a href="https://twitter.com" target='_blank' rel="noreferrer"  className="Twitter">
+                    <FontAwesomeIcon icon={faTwitter} size='2x' />
+                </a>
+                <a href="https://www.instagram.com" target='_blank' rel="noreferrer"  className="Instagram">
+                    <FontAwesomeIcon icon={faInstagramSquare} size='2x' />
+                </a>
+            </section>
+        </div>
+    )
+}
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <section>
+                <h1 className="Title">
+                    WHAT WE COOKING
+                </h1>
+            </section>
+        </div>
+    )
+}
+
+export default Home;
